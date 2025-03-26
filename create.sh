b06f9f69a22f0b1864757d0349cf8fd19ddb1c6c
Instructor <instructor@langara.ca>
2025-03-26 22:16:42 +0000

    Mods

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-mkdir -p continents/{"North America","South America","Asia","Europe"}/{"Air Bases","Ports"}
+mkdir -p continents/{"North America","South America","Asia","Europe"}
 mkdir -p continents/"North America"/{"America","Canada","Mexico"}
 mkdir -p continents/"South America"/{"Brazil","Argintina","Chile","Colombia"}
 mkdir -p continents/"Asia"/{"Japan","China","South Korea","Vietnam","Philipines","Thailand"}
