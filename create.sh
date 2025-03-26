5ed1c0149f26f4ac628a35ee2ec8441d2350d3b5
Instructor <instructor@langara.ca>
2025-03-26 22:15:23 +0000

    Creating directories

---
+++ create.sh
@@ -0,0 +1,9 @@
+mkdir -p continents/{"North America","South America","Asia","Europe"}/{"Air Bases","Ports"}
+mkdir -p continents/"North America"/{"America","Canada","Mexico"}
+mkdir -p continents/"South America"/{"Brazil","Argintina","Chile","Colombia"}
+mkdir -p continents/"Asia"/{"Japan","China","South Korea","Vietnam","Philipines","Thailand"}
+mkdir -p continents/"Europe"/{"Spain","France","Italy","Germany","Switzerland","Belgium","Norway"}
+echo test tet > placeholder
+
+find . -mindepth 2 -type d -exec cp placeholder {} \;
+
